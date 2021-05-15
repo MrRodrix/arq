66dc80dfbe9ad2547a68f42bcb2b5ad87e000a54
Matheus Felipe <m.felipe.1004@gmail.com>
2021-05-14 22:15:14 -0300

    escrevendo resposta em arquivo, sem a string endfile

--- projeto.asm
+++ projeto.asm
@@ -3,6 +3,7 @@
 arquivo_entrada:	.asciiz "teste.txt"
 arquivo_saida:		.asciiz "resposta.txt"
 buffer:			    .space 512
+new_string:                 .space 512
 
 
 .text
@@ -46,47 +47,47 @@ loop:
     beq $t1, 0, fim
     beq $t1, 10, end_verify #verificando se t0 e igual a "endfile"
     #Casos especificos
-    beq $t1, -25, to_upper #? -> ?
-    beq $t1, -57, to_lower #? -> ?
+    beq $t1, -25, to_upper #ç -> Ç
+    beq $t1, -57, to_lower #Ç -> ç
     
     #Agudo ?
-    beq $t1, -31, to_upper #? -> ?
-    beq $t1, -63, to_lower #? -> ?
+    beq $t1, -31, to_upper #á -> Á
+    beq $t1, -63, to_lower #Á -> á
     
-    beq $t1, -23, to_upper #? -> ?
-    beq $t1, -55, to_lower #? -> ?
+    beq $t1, -23, to_upper #é -> É
+    beq $t1, -55, to_lower #É -> é
     
-    beq $t1, -19, to_upper #? -> ?
-    beq $t1, -51, to_lower #? -> ?
+    beq $t1, -19, to_upper #í -> Í
+    beq $t1, -51, to_lower #Í -> í
     
-    beq $t1, -13, to_upper #? -> ?
-    beq $t1, -45, to_lower #? -> ?
+    beq $t1, -13, to_upper #ó -> Ó
+    beq $t1, -45, to_lower #Ó -> ó
     
-    beq $t1, -6, to_upper #? -> ?
-    beq $t1, -38, to_lower #? -> u
+    beq $t1, -6, to_upper  #ú -> Ú
+    beq $t1, -38, to_lower #Ú -> ú
     #Circunflexo ^
     
-    beq $t1, -30, to_upper #? -> ?
-    beq $t1, -62, to_lower #? -> ?
+    beq $t1, -30, to_upper #â -> Â
+    beq $t1, -62, to_lower #Â -> â
     
-    beq $t1, -22, to_upper #? -> ?
-    beq $t1, -54, to_lower #? -> ?
+    beq $t1, -22, to_upper #ê -> Ê
+    beq $t1, -54, to_lower #Ê -> ê
     
-    beq $t1, -18, to_upper #? -> ?
-    beq $t1, -50, to_lower #? -> ? 
+    beq $t1, -18, to_upper #î -> Î
+    beq $t1, -50, to_lower #Î -> î 
     
-    beq $t1, -12, to_upper #? -> ?
-    beq $t1, -44, to_lower #? -> ?
+    beq $t1, -12, to_upper #ô -> Ô
+    beq $t1, -44, to_lower #Ô -> ô
     
-    beq $t1, -5, to_upper #? -> ?
-    beq $t1, -37, to_lower #? -> ?
+    beq $t1, -5, to_upper #û -> Û
+    beq $t1, -37, to_lower #Û -> û
     
     #Til ~ 
-    beq $t1, -29, to_upper #? -> ?
-    beq $t1, -61, to_lower #? -> ?
+    beq $t1, -29, to_upper #ã -> Ã
+    beq $t1, -61, to_lower #Ã -> ã
     
-    beq $t1, -11, to_upper #? -> ?
-    beq $t1, -43, to_lower #? -> ?
+    beq $t1, -11, to_upper #õ -> Õ
+    beq $t1, -43, to_lower #Õ -> õ
     
     
     
@@ -123,6 +124,8 @@ swap:
     lb $t6, buffer($s0) #guarda o caractere de s0 em t6
     sb $t5, buffer($s0) #primeira parte da troca, coloca o caractere de t5 na string na posicao onde estava o caractere de t6
     sb $t6, buffer($t3) #segunda parte da troca, coloca o caractere de t6 na string na posicao onde estava o caractere de t5
+    sb $t5, new_string($s0) #salva o caractere alterado numa nova string(que futuramante será o output)
+    sb $t6, new_string($t3) #salva o caractere alterado numa nova string(que futuramante será o output)
     addi $t3, $t3,1 # avança para o próximo caractere da string(da esquerda pra direita)
     subi $s0, $s0,1 # avança para o próximo caractere da string(da direita pra esquerda)
     j swap # loop
@@ -138,14 +141,18 @@ next_step:
 to_upper:
     sub $t1, $t1, 32 # deixa maiusculo
     sb $t1, buffer($t0)
+    sb $t1, new_string($t0) #salva o caractere alterado numa nova string(que futuramante será o output)
     j next_step      # proxima letra
 
 to_lower:
     add $t1, $t1, 32 # deixa minusculo
     sb $t1, buffer($t0)
+    sb $t1, new_string($t0) #salva o caractere alterado numa nova string(que futuramante será o output)
     j next_step      # proxima letra
 
 end_verify:
+    lb $t1, buffer($t0)
+    sb $t1, new_string($t0) #salva a quebra de linha na nova string(que futuramante será o output)
     addi $t0, $t0, 1
     move $t2, $t0
     
@@ -178,6 +185,8 @@ end_verify:
     #checa se eh endfile
     
 end_verify_reverse:
+    lb $t1, buffer($t0)
+    sb $t1, new_string($t0)
     addi $t0, $t0, 1
     move $t2, $t0
     
@@ -222,8 +231,28 @@ li $v1, 1                   #salvando 1 em $v1
 
 #printando string 
 li $v0, 4          
-la $a0, buffer
+la $a0, new_string
 syscall
+
+#abrir arquivo
+li $v0, 13			        #codigo para abrir arquivo
+la $a0, arquivo_saida		#salvando nome de arquivo que queremos abrir no registrador $a0
+li $a1, 1			        #escolhendo modo de escrita
+syscall                     #chamando funcao
+move $s1, $v0			    #passando o arquivo de v0 para s1
+
+#escrever o arquivo
+li $v0, 15			        #codigo para escrever arquivo
+move $a0, $s1			    #passando os bytes de s1 como parametro da funcao 
+la $a1, new_string			    #passando o buffer de output
+li $a2, 512			        #tamanho de buffer definido em .data
+syscall                     #chamando funcao
+
+#fechar arquivo
+li $v0, 16			        #codigo para fechar arquivo
+move $a0, $s1			    #passando como parametro o arquivo aberto
+syscall
+
 li $v0, 10
 syscall
 #exemplo file:
